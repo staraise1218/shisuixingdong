18641ab00fe63bd29bb055733835e88350c738b0
shengli <363912809@qq.com>
2018-08-24 17:38:35 +0800

    email

--- application/extra/site.php
+++ application/extra/site.php
@@ -27,12 +27,12 @@ return array (
     'example' => 'Example',
   ),
   'mail_type' => '1',
-  'mail_smtp_host' => 'smtp.qq.com',
+  'mail_smtp_host' => 'smtp.163.com',
   'mail_smtp_port' => '465',
-  'mail_smtp_user' => '10000',
-  'mail_smtp_pass' => 'password',
+  'mail_smtp_user' => 'shengli363',
+  'mail_smtp_pass' => 'wsl664782835',
   'mail_verify_type' => '2',
-  'mail_from' => '10000@qq.com',
+  'mail_from' => 'shengli363@163.com',
   'copyright' => 'Copyright © 2016-2025 中垦优选 版权所有 保留一切权力',
   'feedback' => '<p>尊敬的XX阁下，您好！</p>
 <p>移动设备优先是 Bootstrap 3 的最显著的变化。在之前的版本中（直到2.x），您需要手动引用另一个CSS，才能让整个项目友好的支持移动设备。</p>
